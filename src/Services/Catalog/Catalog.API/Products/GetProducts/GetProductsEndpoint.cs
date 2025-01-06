78397f24ceb53d2c0001bf31a311ed25ec09bd18
lovindy <seangdy085@gmail.com>
2025-01-05 19:57:34 -0800

    Setup the docker host

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    //public record GetProductsRequest;
+    public record GetProductsResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            // Define the endpoint to get all products.
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                // Send the GetProductsQuery record to the GetProductsQueryHandler.
+                var result = await sender.Send(new GetProductsQuery());
+                // Map the GetProductsResult record to the HTTP response.
+                var response = new GetProductsResponse(result.Products);
+                // Return the HTTP response.
+                return Results.Ok(response);
+            });
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler
+        (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting products from the database...");
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
+    }
+}
