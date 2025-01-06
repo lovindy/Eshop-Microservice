b2f5d871e4cd41e0a4a3e19af926d2474c1198b5
lovindy <seangdy085@gmail.com>
2025-01-05 22:32:15 -0800

    Implement the get product by Id

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,32 @@
+﻿
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.NewFolder
+{
+
+    //public record GetProductByIdRequest();
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            // Define the endpoint to get a product by id.
+            app.MapGet("/products/{id}", async (ISender sender, string id) =>
+            {
+                // Send the GetProductsQuery record to the GetProductsQueryHandler.
+                var result = await sender.Send(new GetProductsQuery { Id = id });
+
+                // Map the GetProductsResult record to the HTTP response.
+                var response = result.Adapt<GetProductByIdResponse>();
+
+                // Return the HTTP response.
+                return Results.Ok(response);
+            })
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get product by id.")
+            .WithDescription("Get product by id.");
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -8,10 +8,15 @@ namespace Catalog.API.Products.GetProducts
         (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
         : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
-        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
+            // Log that we are getting products from the database.
             logger.LogInformation("Getting products from the database...");
+
+            // Get all products from the database.
             var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            // Return the products.
             return new GetProductsResult(products);
         }
     }
