70654731ff14ce62de8f5449f058d0aabf5cf68b
lovindy <seangdy085@gmail.com>
2025-01-15 22:33:17 -0800

    Setup the logging behavior

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse>
+        (ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull, IRequest<TResponse>
+        where TResponse : notnull
+    {
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
