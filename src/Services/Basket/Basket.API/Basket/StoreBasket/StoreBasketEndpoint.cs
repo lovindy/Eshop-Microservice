11c8495a7c12460eac0a0ea560ed3f9271d12ba4
lovindy <seangdy085@gmail.com>
2025-01-17 20:28:46 -0800

    Implemented the Post endpoint of the StoreBasket

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -7,7 +7,25 @@ namespace Basket.API.Basket.StoreBasket
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                // Map the request to the command
+                var command = request.Adapt<StoreBasketCommand>();
+
+                // Store the basket
+                var result = await sender.Send(command);
+
+                // Map the result to the response
+                var response = result.Adapt<StoreBasketResponse>();
+
+                // Return the response
+                return Results.Created($"/basket/{response.Username}", response);
+            })
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Store the basket for the specified user")
+            .WithDescription("Stores the basket for the specified user.");
         }
     }
 }
