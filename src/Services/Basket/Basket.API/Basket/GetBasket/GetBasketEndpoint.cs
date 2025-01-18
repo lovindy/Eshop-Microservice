ebd3eaedd2a5c6a020caf22b05f32d2c11034e7b
lovindy <seangdy085@gmail.com>
2025-01-17 20:04:15 -0800

    Complete the get endpont of the basket

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -11,11 +11,20 @@
             // Implement the GetBasket endpoint
             app.MapGet("/basket/{userName}", async (string userName, ISender Sender) =>
             {
+                // Get the basket from the database
+                var result = await Sender.Send(new GetBasketQuery(userName));
 
+                // Map the result to the response
+                var response = result.Adapt<GetBasketResponse>();
 
-            });
-
-
+                // Return the response
+                return Results.Ok(response);
+            })
+            .WithName("GetBasket")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get the basket for the specified user")
+            .WithDescription("Returns the basket for the specified user.");
         }
     }
 }
--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -2,3 +2,4 @@
 global using BuildingBlocks.CQRS;
 global using Carter;
 global using MediatR;
+global using Mapster;
