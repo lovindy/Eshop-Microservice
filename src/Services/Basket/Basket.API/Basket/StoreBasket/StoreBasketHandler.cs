4a91eee3a1f78fa7a25d7a28f7b87af61b9d455e
lovindy <seangdy085@gmail.com>
2025-01-17 20:11:14 -0800

    Implement the record and validation

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using FluentValidation;
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(bool IsSuccess);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null.");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("Username is required.");
+        }
+    }
+
+    public class StoreBasketCommandHandler
     {
     }
 }
